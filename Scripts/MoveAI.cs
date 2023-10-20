0acc11a5295d76ab16d558bb89bf27f07de3a354
Cerubois <55885923+Cerubois@users.noreply.github.com>
2023-10-20 13:41:04 -0400

    Oct 14 class

--- Scripts/MoveAI.cs
+++ Scripts/MoveAI.cs
@@ -3,12 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MoveAI : Agent
 {
 
+	public Vector3 startPos = Vector3.zero;
+	public float moveSpeed = 1.0f;
+	[SerializeField]
+	private Transform goalTransform;
+
+	public override void OnEpisodeBegin() {
+		transform.localPosition = startPos;
+	}
+
+	public override void CollectObservations(VectorSensor sensor) {
+		sensor.AddObservation(transform.localPosition);
+		sensor.AddObservation(goalTransform.localPosition);
+	}
+
 	public override void OnActionReceived(ActionBuffers actions) {
-		Debug.Log(actions.DiscreteActions[0]);
+		//Debug.Log(actions.DiscreteActions[0]);
+		//Debug.Log(actions.ContinuousActions[0]);
+
+		float moveX = actions.ContinuousActions[0];
+		float moveZ = actions.ContinuousActions[1];
+
+		transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		
+		if(other.transform != null) {
+
+			if(other.transform.tag == "Target") {
+
+				SetReward(1f);
+				EndEpisode();
+
+			}
+			else if(other.transform.tag == "Wall") {
+
+				EndEpisode();
+
+			}
+
+		}
+
 	}
 
 }
